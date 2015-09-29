adac2ae10d2d66b4374089e6f4200844ad8babd1
kasalolek <kasalolek@gmail.com>
2015-09-29 17:51:21 +0200

    zadanie 3

---
+++ src/main/java/emilianpukacz/App.java
@@ -0,0 +1,35 @@
+package emilianpukacz;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner keyboardInput = new Scanner(System.in);
+        String filename = keyboardInput.nextLine();
+
+        App obj = new App();
+        String zawartoscPliku = obj.getFileFromResources(filename);
+        System.out.println("oryginal: "+zawartoscPliku);
+        String zawartoscPlikuBezSpacji = StringUtils.deleteWhitespace(zawartoscPliku);
+        System.out.println("bez spacji: "+zawartoscPlikuBezSpacji);
+    }
+
+    private String getFileFromResources(String filename){
+        ClassLoader classLoader = getClass().getClassLoader();
+        String zawartoscPliku = "";
+        try {
+            zawartoscPliku = IOUtils.toString(classLoader.getResourceAsStream(filename));
+
+        }catch(FileNotFoundException fe){System.out.println("nie znaleziono pliku: "+ filename );}
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return zawartoscPliku;
+    }
+}
