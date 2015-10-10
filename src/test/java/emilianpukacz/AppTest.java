5fbd603c5dd1daef1c686999d53d23bffd225f11
gmakosa <grzegorz.makosa@sagiton.pl>
2015-10-10 19:57:54 +0200

    Dodano komentarze

--- src/test/java/emilianpukacz/AppTest.java
+++ src/test/java/emilianpukacz/AppTest.java
@@ -10,13 +10,13 @@ import static org.junit.Assert.assertNull;
 public class AppTest{
     App app;
 
-String przyklad ="kilka przyskladowych slow a a  aaa v s ss ddd";
-String filename = "przyklad.txt";
+    String przyklad ="kilka przyskladowych slow a a  aaa v s ss ddd";
+    String filename = "przyklad.txt";
+
     @Before
     public void setUp(){
         this.app = new App();
     }
-
 
     @Test
     public void testGetFileFromResources() throws Exception {
@@ -24,4 +24,19 @@ String filename = "przyklad.txt";
         assertEquals(app.getFileFromResources(filename), przyklad);
     }
 
+    /*
+    Kilka komentarzy:
+     - po pierwsze, twoja App ma metodę main(), która woła po kolei wszystkie metody, zbiera dane z klawiatury
+      lub wypisuje zawartość na ekran. A nie jest testowana w ogóle
+     - metoda main jedyne co powinna robić to tworzyć instancję App i uruchamiać ją (np. metoda process, run itp.)
+     - metodę, którą testujesz, czyli getFileFromResources zrobiłeś publiczną by móc ją testować. To nie jest dobry
+      pomysł by zmieniać enkapsujację tylko z powodu testu
+     - nie testujesz tego co aplikacja robi, bo jedynie sprawdzasz czy pobiera się zawartość pliku
+     - podpowiedzi: użyj Mockito by zasymulować Input i Output. Przekonfiguruj App by przyjmowała w konstruktorze
+       input i output (np. BufferedReader i OutputStream) a w domyślnym konstruktorze używała domyślnych systemowych
+       input i output
+     - sprawdź czy na output wypisywane są odpowiednie rezultaty, czy aplikacja wypisuje na output to, co byś chciał
+     - do testów użyj plików w katalogu test/resources
+
+     */
 }
